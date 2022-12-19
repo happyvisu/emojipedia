4e0cf6f8b8680df64d5492c7ba2f84ea4fc79865
happyvisu <happyvisu@yahoo.com>
2022-12-19 15:34:37 +0530

    Update app/index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojiTerm) {
-  return <Entry key={emojiTerm.id}
-                emoji={emojiTerm.emoji}
-                name={emojiTerm.name}
-                description={emojiTerm.meaning} />
-}
+// function createEntry(emojiTerm => (<Entry key={emojiTerm.id}
+//                 emoji={emojiTerm.emoji}
+//                 name={emojiTerm.name}
+//                 description={emojiTerm.meaning} />))
 
 function App(){
   return (
@@ -15,9 +13,12 @@ function App(){
       <h1>
         <span>emojipedia</span>
       </h1>
-
+      
       <dl className="dictionary">
-        {emojipedia.map(createEntry)}        
+        {emojipedia.map(createEntry => <Entry key={createEntry.id}
+                emoji={createEntry.emoji}
+                name={createEntry.name}
+                description={createEntry.meaning} />)}        
       </dl>
     </div>
   );
